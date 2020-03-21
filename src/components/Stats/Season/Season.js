23218042e297f616e5fae466b7aaa3162513f96c
Pantu Andreea <andreea.pantu@gmail.com>
2020-03-21 18:58:50 +0200

    Season and Episode components

---
+++ src/components/Stats/Season/Season.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Episode from "././Episode/Episode.js";
+
+const season = (props) => {
+    return <div className="Season">
+        <p>{props.season.seasonName}</p>
+          {props.season.episode.map(episode => {
+        return <Episode 
+        episodeName={episode.episodeName} 
+        episodeDescription={episode.episodeDescription}
+        episodeDuration={episode.episodeDuration}
+        key={episode.episodeName}
+        />
+        }) 
+        
+        }
+    </div>
+
+}
+
+export default season;
