b0894a1cab51ffc3f99fbf9c9ac288ae642b218d
Andrei Oprin <oprin.andrei@gmail.com>
2020-03-21 18:28:51 +0200

    project strucutre

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import Stats from './components/Stats/Stats';
+
 function App() {
   return (
     <div className="App">
---
+++ src/components/Stats/Stats.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Stats extends Component {
+  render() {
+    return (
+      <h1></h1>
+    );
+  }
+}
+
+export default Stats;
